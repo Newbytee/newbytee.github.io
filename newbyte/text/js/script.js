07b41afcc573d44e8f7f8560126b875788e22d09
Kir Neboula <newbie13xd@outlook.com>
2018-03-04 21:49:17 +0100

    bleeh

--- newbyte/text/js/script.js
+++ newbyte/text/js/script.js
@@ -1,21 +1,64 @@
 var loadingText = document.getElementById("loading");
-var storyText = document.getElementById("storyText");
-var fileReader;
-var tmpTxt;
+var storyText = [ document.getElementById("storyText1"), document.getElementById("storyText2"), document.getElementById("storyText3") ];
 var inventory = [ "Flower" ];
+var currentLocation = "intro_0";
+var storyTextLength = storyText.length;
+var inventorySize = inventory.length;
 
 loadingText.parentNode.removeChild(loadingText);
 
-function readTxtFile(path) {
+function readTxtFile(path, field) {
+
+    if (field === undefined) {
+
+        field = 0;
+
+    }
 
     $.ajax({
-        url : path + ".txt",
+
+        url : "txt/" + path + ".html",
         dataType: "text",
-        success : function (data) {
-            $(storyText).html(data);
+        success : function(data) {
+
+            $(storyText[field]).html(data);
+
         }
+
     });
 
 }
 
-readTxtFile("txt/bleh");
+function changeLocation(locationName) {
+
+    currentLocation = locationName;
+
+    readTxtFile(currentLocation, 0);
+
+}
+
+function displayInventory() {
+
+    inventorySize = inventory.length;
+
+    for (var i = 0; i < inventorySize; i++) {
+
+
+
+    }
+
+}
+
+function clearFields() {
+
+    for (var i = 0; i < storyTextLength; i++) {
+
+        $(storyText[i].html(" "));
+
+    }
+
+}
+
+readTxtFile(currentLocation, 0);
+//clearFields();
+//readTxtFile("txt/bleh", 0);
