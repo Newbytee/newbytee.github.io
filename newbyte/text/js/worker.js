997b179e104725b11fe63f220fe6b97d5f62c57f
Kir Neboula <newbie13xd@outlook.com>
2018-03-12 20:45:10 +0100

    bleeh

---
+++ newbyte/text/js/worker.js
@@ -0,0 +1,13 @@
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.register('js/worker.js').then(function(registration) {
+        console.log('Service worker registration succeeded: ', registration);
+    }).catch(function(error) {
+        console.log('Service worker registration failed: ', error);
+    });
+
+} else {
+
+    console.log('Service workers are not supported');
+
+}
