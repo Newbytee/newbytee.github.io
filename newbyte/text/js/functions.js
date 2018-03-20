bd9de51fc584ae77c78082a31fe92a190695dca5
Kir Neboula <newbie13xd@outlook.com>
2018-03-20 17:49:27 +0100

    bleeh

---
+++ newbyte/text/js/functions.js
@@ -0,0 +1,125 @@
+var currentSheet = document.getElementById("pageTheme");
+var loadingText = document.getElementById("loading");
+var storyText = [ document.getElementById("storyText1"), document.getElementById("storyText2"), document.getElementById("storyText3") ];
+var inventory = [];
+var currentLocation;
+var visitedLocations = [];
+var storyTextLength = storyText.length;
+var inventorySize = inventory.length;
+
+function readTxtFile(path, field) {
+
+    if (field === undefined) {
+
+        field = 1;
+
+    }
+
+    $.ajax({
+
+        url : path,
+        dataType: "text",
+        success : function(data) {
+
+            $(storyText[field]).html(data);
+
+        }
+
+    });
+
+}
+
+function changeLocation(locationName) {
+
+    localStorage.setItem("currentLocation", locationName);
+    visitedLocations.push(locationName);
+    currentLocation  = "txt/" + locationName + ".html";
+
+    readTxtFile(currentLocation, 0);
+
+}
+
+function displayInventory() {
+
+    inventorySize = inventory.length;
+
+    clearFields(2);
+
+    if (inventorySize === 0) {
+
+        $(storyText[2]).html("Your inventory is empty");
+
+        return;
+
+    }
+
+    var tmpInventory = "Your inventory contains:<br>";
+
+    for (var i = 0; i < inventorySize; i++) {
+
+        tmpInventory += inventory[i] + "<br>";
+
+    }
+
+    $(storyText[2]).html(tmpInventory);
+
+}
+
+function addToInventory(item) {
+
+    inventory.push(item);
+    localStorage.setItem("inventory", JSON.stringify(inventory));
+
+}
+
+function clearFields(n) {
+
+    if (isNaN(n)) {
+
+        for (var i = 0; i < storyTextLength; i++) {
+
+            $(storyText[i]).html(" ");
+
+        }
+
+    } else {
+
+        $(storyText[n]).html(" ");
+
+    }
+
+}
+
+function resetProgress() {
+
+    if (confirm("Are you sure? This will reset all your progress")) {
+
+        localStorage.removeItem("currentLocation");
+        localStorage.removeItem("inventory");
+        location.reload();
+
+    }
+
+}
+
+function switchTheme(theme) {
+
+    var sheetSwitchButton = document.getElementById("themeSwitchButton");
+
+    localStorage.setItem("theme", theme);
+    currentSheet.setAttribute("href", "css/" + theme + ".css");
+
+    if (theme === "dark") {
+
+        sheetSwitchButton.setAttribute("onclick", "switchTheme('light')");
+        sheetSwitchButton.innerHTML = "Switch to light mode";
+
+
+    } else {
+
+        sheetSwitchButton.setAttribute("onclick", "switchTheme('dark')");
+        sheetSwitchButton.innerHTML = "Switch to dark mode";
+
+    }
+
+}
--- newbyte/text/js/script.js
+++ newbyte/text/js/script.js
@@ -1,11 +1,3 @@
-var loadingText = document.getElementById("loading");
-var storyText = [ document.getElementById("storyText1"), document.getElementById("storyText2"), document.getElementById("storyText3") ];
-var inventory = [];
-var currentLocation;
-var visitedLocations = [];
-var storyTextLength = storyText.length;
-var inventorySize = inventory.length;
-
 if (!(storageAvailable('localStorage'))) {
 
     alert("Warning: The local storage API seems to be unavailable on your current device/browser, saving may not be available.");
@@ -31,100 +23,7 @@ inventory = JSON.parse(localStorage.getItem("inventory"));
 
 console.log(currentLocation);
 
-function readTxtFile(path, field) {
 
-    if (field === undefined) {
-
-        field = 1;
-
-    }
-
-    $.ajax({
-
-        url : path,
-        dataType: "text",
-        success : function(data) {
-
-            $(storyText[field]).html(data);
-
-        }
-
-    });
-
-}
-
-function changeLocation(locationName) {
-
-    localStorage.setItem("currentLocation", locationName);
-    visitedLocations.push(locationName);
-    currentLocation  = "txt/" + locationName + ".html";
-
-    readTxtFile(currentLocation, 0);
-
-}
-
-function displayInventory() {
-
-    inventorySize = inventory.length;
-
-    clearFields(2);
-
-    if (inventorySize === 0) {
-
-        $(storyText[2]).html("Your inventory is empty");
-
-        return;
-
-    }
-
-    var tmpInventory = "Your inventory contains:<br>";
-
-    for (var i = 0; i < inventorySize; i++) {
-
-        tmpInventory += inventory[i] + "<br>";
-
-    }
-
-    $(storyText[2]).html(tmpInventory);
-
-}
-
-function addToInventory(item) {
-    
-    inventory.push(item);
-    localStorage.setItem("inventory", JSON.stringify(inventory));
-    
-}
-
-function clearFields(n) {
-
-    if (isNaN(n)) {
-
-        for (var i = 0; i < storyTextLength; i++) {
-
-            $(storyText[i]).html(" ");
-
-        }
-
-    } else {
-
-        $(storyText[n]).html(" ");
-
-    }
-
-}
-
-function resetProgress() {
-
-    if (confirm("Are you sure? This will reset all your progress")) {
-        
-        localStorage.removeItem("currentLocation");
-        localStorage.removeItem("inventory");
-        location.reload();
-        
-    }
-    
-}
 
 loadingText.parentNode.removeChild(loadingText);
 
