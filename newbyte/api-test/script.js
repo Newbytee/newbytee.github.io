46b523cd5dda084d6afa4e5bd58104f4dc9e746d
Kir Neboula <newbie13xd@outlook.com>
2018-05-23 21:29:50 +0200

    meep

--- newbyte/api-test/script.js
+++ newbyte/api-test/script.js
@@ -2,17 +2,32 @@
 
 const switchButton = document.getElementById("toggleButton");
 let vibrate = false;
+let intervalVar;
 
 switchButton.addEventListener("click", function() {
-    if (vibrate === true) {
-        vibrate = false;
-    } else {
-        vibrate = true;
-        setInterval(function() {
+    if (vibrate) {
+        intervalVar = setInterval(function() {
             window.navigator.vibrate(10);
             console.log("done");
         }, 10);
+        vibrate = false;
+    } else {
+        clearInterval(intervalVar);
+        vibrate = true;
     }
+
+
+
+    /*if (vibrate === true) {
+        vibrate = false;
+        intervalVar
+    } else {
+        vibrate = true;
+        let intervalVar = setInterval(function() {
+            window.navigator.vibrate(10);
+
+        }, 10);
+    }*/
 
     //window.navigator.vibrate([200, 100, 200]);
 
